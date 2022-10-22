b641613f47e491d5f1524ecffe1294e80ea1bfa5
Chloe Yeo <yeochloe01@gmail.com>
2022-10-22 23:32:17 +0100

    various types of tasks added

--- ecohouse/pages/api/form.js
+++ ecohouse/pages/api/form.js
@@ -1,20 +1,20 @@
 const TaskForm = () => (
         <div class="max-w-2xl mx-auto">
-            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select an option</label>
+            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Choose the type of task</label>
                 <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option selected>Choose a country</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
+                <option selected>Select option</option>
+                <option value="Laundry">Laundry</option>
+                <option value="Wash Dish">Wash Dishes</option>
+                <option value="Wash Dish">Wash Fruits and Vegetables</option>
+                <option value="Wash Dish">Take Shower</option>
+                <option value="Wash Dish">Water Plants</option>
+                <option value="Make Tea">Make Tea/Coffee</option>
+                <option value="Make Tea">Use Oven</option>
+                <option value="Make Tea">Vacuum</option>
+                <option value="Make Tea">Cook</option>
+                <option value="Boil Kettle">Boil Kettle</option>
+                <option value="Make Tea">Shop Groceries</option>
             </select>
-
-            <p class="mt-5">This select component is part of a larger, open-source library of Tailwind CSS components.
-                Learn
-                more
-                by going to the official <a class="text-blue-600 hover:underline"
-                    href="https://flowbite.com/docs/getting-started/introduction/" target="_blank">Flowbite Documentation</a>.
-            </p>
             <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
         </div>
 
