598d58985e577244da2749e1d4c1e93b32a30699
mynameistessa <tessamusic18@outlook.com>
2022-10-23 02:36:15 +0100

    Add modal content

--- ecohouse/components/form/Modal.js
+++ ecohouse/components/form/Modal.js
@@ -1,15 +1,47 @@
 import { useTask } from '../../context/task-context';
 
 const Modal = () => {
-    const {addTask} = useTask()
-    return(
-    <div className="w-full h-screen flex items-center justify-center">
-     <div className="bg-white rounded p-4 z-30 w-64 h-64">Whats up</div>
-     <button onClick={() => addTask()}>
-        Add
-     </button>
-  </div>
-)}
+  const { addTask } = useTask()
+  return (
+    <div className="w-full h-screen flex items-center justify-center modal">
+      <div className="bg-white rounded p-4 z-30 w-64 h-64" >
+        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel">Add event</h5>
+        <label for="tasks" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400" >Select activity</label>
+        <select id="activities" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option value="Laundry">Laundry</option>
+          <option value="Wash Dish">Wash Dishes</option>
+          <option value="Wash Veg">Wash Fruits and Vegetables</option>
+          <option value="Shower">Take Shower</option>
+          <option value="Water Plant">Water Plants</option>
+          <option value="Make Drink">Make Tea/Coffee</option>
+          <option value="Use Oven">Use Oven</option>
+          <option value="Vacuum">Vacuum</option>
+          <option value="Cook">Cook</option>
+          <option value="Boil Kettle">Boil Kettle</option>
+          <option value="Shop Groc">Shop Groceries</option>
+        </select>
+        <div class="flex justify-center">
+          <div class="timepicker relative form-floating mb-3 xl:w-96">
+            <label for="floatingInput" class="text-gray-700">Select a date</label>
+            <input type="text"
+              class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Select a date" />
+          </div>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
+          <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+            Recurring activity
+          </label>
+        </div>
+        <button onClick={() => addTask()} className="z-31 block add-button modal-footer justify-end p-4">
+          Add
+        </button>
+
+      </div>
+    </div>
+  )
+}
 
 
 export default Modal
--- ecohouse/pages/_app.js
+++ ecohouse/pages/_app.js
@@ -1,15 +1,11 @@
 import '../styles/globals.css'
 import {TaskProvider} from "../context/task-context"
-{/* <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.4/dist/flowbite.min.css" /> */}
 
 function MyApp({ Component, pageProps }) {
   return (
-  //   <>
     <TaskProvider>
       <Component {...pageProps} />
-      </TaskProvider>
-    //   <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.4/dist/flowbite.min.css" />
-    // </>
+    </TaskProvider>
   )
 }
 
