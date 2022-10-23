8731b5543499ee64f417b5575deae52ae79af54f
mynameistessa <tessamusic18@outlook.com>
2022-10-23 02:42:07 +0100

    make modal fonts consistent

--- ecohouse/components/form/Modal.js
+++ ecohouse/components/form/Modal.js
@@ -4,8 +4,8 @@ const Modal = () => {
   const { addTask } = useTask()
   return (
     <div className="w-full h-screen flex items-center justify-center modal">
-      <div className="bg-white rounded p-4 z-30 w-64 h-64" >
-        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel">Add event</h5>
+      <div className="bg-white rounded p-4 z-30 w-64 h-84" >
+        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel">Add Calendar Event</h5>
         <label for="tasks" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400" >Select activity</label>
         <select id="activities" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option value="Laundry">Laundry</option>
@@ -22,19 +22,17 @@ const Modal = () => {
         </select>
         <div class="flex justify-center">
           <div class="timepicker relative form-floating mb-3 xl:w-96">
-            <label for="floatingInput" class="text-gray-700">Select a date</label>
+            <label for="floatingInput" class="text-sm font-medium text-gray-900">Select a date</label>
             <input type="text"
-              class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              class="form-control block w-full px-3 py-1.5 text-sm font-medium text-gray-900 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Select a date" />
           </div>
         </div>
-        <div class="form-check">
-          <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
-          <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
-            Recurring activity
-          </label>
-        </div>
-        <button onClick={() => addTask()} className="z-31 block add-button modal-footer justify-end p-4">
+        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
+        <label class="mb-2 form-check-label inline-block text-sm text-gray-900" for="flexCheckDefault">
+          Recurring activity
+        </label>
+        <button onClick={() => addTask()} className="z-31 p-6 block add-button modal-footer justify-end">
           Add
         </button>
 
